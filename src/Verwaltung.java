2d4bc75a2b946dd54617533ba6b04fb169859a05
pulquepara2 <123812696+pulquepara2@users.noreply.github.com>
2023-04-13 15:52:09 +0200

    neu

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Verwaltung {
+    private ArrayList<Player> players = new ArrayList<>();
+
+    public void add(Player p){
+        players.add(p);
+    }
+    public void remove(Player p){
+        players.remove(p);
+    }
+    public Player getPlayerById(int id){
+        Player result = players.get(0);
+        int playerWithId = 0;
+        for(Player p : players){
+            if(p.getId() == id){
+                playerWithId = p.getId();
+                result = p;
+            }
+        }
+     return result;
+    }
+
+    public int getPointsPerTeam(Team team){
+        int pointsperTeam = 0;
+
+    }
+    public Player getWinner(){
+
+    }
 }
