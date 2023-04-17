8a5fc205272ccf731a655d19321a31293ade8108
pulquepara2 <123812696+pulquepara2@users.noreply.github.com>
2023-04-17 14:31:33 +0200

    neuneu

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+
 public class WordCount {
+    private String text;
+    private HashMap<String, Integer> wordcount = new HashMap<>();
+
+    public WordCount(String text) {
+        this.text = text;
+    }
+
 }
