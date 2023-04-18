7413a52ae3cd91bb35dfa4f10d978ea87d034343
pulquepara2 <123812696+pulquepara2@users.noreply.github.com>
2023-04-18 13:51:45 +0200

    neuneu

--- src/ProduktDEmo.java
+++ src/ProduktDEmo.java
@@ -2,18 +2,29 @@ public class ProduktDEmo {
     public static void main(String[] args) {
 
 
-        Produkt produkt1 = new Produkt("Produkt 1", 1200);
+        Produkt produkt1 = new Produkt("Produkt 1", 800);
         produkt1.setDiscount(20);
         produkt1.setDescription("Produkt 1 aus Shop A");
         System.out.println(produkt1.getPriceWithDiscount());
         System.out.println(produkt1);
 
-        Produkt p2 = new Produkt("Produkt 1", 1200);
-        p2.setDescription("Produkt 1 aus Shop B");
+        Produkt p2 = new Produkt("Produkt 2", 1250);
+        p2.setDescription("Produkt 2 aus Shop B");
         p2.setDiscount(10);
         System.out.println(p2.getPriceWithDiscount());
         System.out.println(p2);
         System.out.println(produkt1.equals(p2));
+
+        ProduktShop shop = new ProduktShop();
+        shop.add(produkt1);
+        shop.add(p2);
+        System.out.println(shop.gibtsGratis());
+        System.out.println(shop.getPriceFrom(1000));
+        System.out.println(shop.getPriceTo(800));
+
+        System.out.println(shop.toString());
+        shop.removeProductsFromByIterator(1000);
+        System.out.println(shop.toString());
 
     }
 }
--- src/ProduktShop.java
+++ src/ProduktShop.java
@@ -1,3 +1,88 @@
-public class ProduktShop {
+import java.util.*;
 
+public class ProduktShop {
+    HashMap<String, Produkt> produkte = new HashMap<>();
+
+    @Override
+    public String toString() {
+        return "ProduktShop{" +
+                "produkte=" + produkte +
+                '}';
+    }
+
+    public void add(Produkt p) {
+        produkte.put(p.getTitle(), p);
+    }
+
+    public Produkt get(String title) {
+        Produkt p = produkte.get(title);
+        if (p == null) {
+            System.out.println("Nicht gefunden");
+        }
+
+        return p;
+    }
+
+    public boolean gibtsGratis() {
+        for (Produkt p : produkte.values()) {
+            if (p.getDiscount() == 100) {
+                return true;
+            }
+        }
+        for (String key : produkte.keySet()) {
+            Produkt p = produkte.get(key);
+            if (p.getDiscount() == 100) {
+                return true;
+            }
+        }
+        for (Map.Entry<String, Produkt> e : produkte.entrySet()) {
+            Produkt p = e.getValue();
+            if (p.getDiscount() == 100) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Produkt> getPriceFrom(double from) {
+        ArrayList<Produkt> result = new ArrayList<>();
+        for (Produkt p : produkte.values()) {
+            if (p.getPrice() >= from) {
+                result.add(p);
+            }
+
+        }
+        return result;
+    }
+
+    public ArrayList<Produkt> getPriceTo(double to) {
+        ArrayList<Produkt> result = new ArrayList<>();
+        for (Produkt p : produkte.values()) {
+            if (p.getPrice() <= to) {
+                result.add(p);
+            }
+
+        }
+        return result;
+    }
+
+    public void removeProductsFromByIterator(double from) {
+        Iterator<Map.Entry<String, Produkt>> it = produkte.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Produkt> e = it.next();
+            Produkt p = e.getValue();
+            if (p.getPrice() > from) {
+                it.remove();
+            }
+        }
+    }
+    //beide Methoden machen das gleiche
+    public void removeProductsByKeySet(double from){
+        Set<String> keys = new HashSet<>();
+        for(Produkt p : produkte.values()){
+            if(p.getPrice() > from){
+                keys.add(p.getTitle());
+            }
+        }
+    }
 }
